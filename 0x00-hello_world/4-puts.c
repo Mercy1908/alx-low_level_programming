c07000d4f1db9e307c6826b62a849abbd343eb3e
Mercy1908 <frankmercy806@gmail.com>
2023-03-16 16:57:34 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+* main - print a string using puts function
+*
+* Description: using the main func
+* This program prints Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main (void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+}
