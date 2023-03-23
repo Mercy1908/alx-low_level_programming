33f9c110f59b4cd3d13abd701e9e1f2ba11a0640
Mercy1908 <frankmercy806@gmail.com>
2023-03-22 17:19:35 -0700

    Alphabet 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
-{
-	int i;
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
+{       
+        int i;
+        
+        for (i = 97; i <= 122; i++)
+        {       
+                _putchar(i);
+        }       
+        _putchar('\n');
+}       
